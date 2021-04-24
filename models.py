d5c4e0471b462704d4c34d01c9c1439ee71b98d6
myportfolio-tech <eduardo@myportfolio.com>
2021-04-24 10:43:12 -0400

    Changes made to models

--- models.py
+++ models.py
@@ -18,7 +18,7 @@ class Venue(db.Model):
     website_link = db.Column(db.String(120))
     seeking_talent = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
-    artists = db.relationship('Artist', secondary='shows', backref=db.backref('venue', lazy=True))
+    shows = db.relationship('Show', backref="venue", lazy=True)
 
 
 
@@ -41,14 +41,16 @@ class Artist(db.Model):
     website_link = db.Column(db.String(120))
     seeking_venue = db.Column(db.Boolean)
     seeking_description = db.Column(db.String(120))
+    shows = db.relationship('Show', backref="artist", lazy=True)
 
 
 class Show(db.Model):
   __tablename__ = 'shows'
   
-  artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'), primary_key=True)
-  venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'), primary_key=True)
+  id = id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column(db.Integer, db.ForeignKey('artists.id'))
+  venue_id = db.Column(db.Integer, db.ForeignKey('venues.id'))
   start_time = db.Column(db.DateTime, default=datetime.now())
 
 
-#db.create_all()
+#db.create_all()flask db 
